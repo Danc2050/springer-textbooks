1c348b8cd7f411588ce6ea486b7279476150d89c
Danc2050 <danc2@pdx.edu>
2020-04-24 21:03:31 -0700

    fixed duplicates (thanks, Teal Dulcet)

--- download.py
+++ download.py
@@ -1,10 +1,10 @@
-# downloads all the e-books
 from bs4 import BeautifulSoup
 import re
 import requests
 import time
 import subprocess
 import os
+import wget
 
 if not os.path.exists("ebooks"):
     os.mkdir("ebooks")
@@ -15,12 +15,16 @@ with open("../books.txt", "r") as f1:
     for line in f1:
         page = requests.get(line)
         soup = BeautifulSoup(page.content, 'html.parser')
-        title = soup.find_all('title')[0].text.split("/")[0] # wget has issues renaming file with a `/`. We truncate the filename at this point if one exists.
+
+        # wget has issues renaming file with a `/`. We replace it with a ' ' if a '/' exists.
+        title = soup.title.text.split("| SpringerLink")[0].replace("/", " ").rstrip()
+        #authors = soup.find(class_="authors__name").text # optionally, add this to var output
+        isbn = soup.find(id="print-isbn").text
+        output = f'{title} {isbn}.pdf'
         search_string = str(soup.find_all('div', class_='cta-button-container__item')[0])
         for i in range(0, len(search_string)):
-            if search_string[i]=='h' and search_string[i+1] == 'r' and search_string[i+2] == 'e':
+            if search_string[i]=='h' and search_string[i+1] == 'r' and search_string[i+2] == 'e': # probably a better way to do this
                 tail_url = search_string[i+6:-1].split(" ")[0]
                 full_url = base_url + tail_url
                 print(f'Downloading {full_url}')
-                output = subprocess.check_output(f'wget -cO \"{title}\" \"{full_url}', shell=True).decode()
-        time.sleep(5)
+                wget.download(full_url, out=output)
